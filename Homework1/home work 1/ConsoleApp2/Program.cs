8b67439485d5fe82bee130be46176a2a3fb12438
ToughSpirit <greeeennn@gmail.com>
2023-01-14 19:18:28 +0600

    1

---
+++ Homework1/home work 1/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+            Write("Hell");
+            Write("o, ");
+            Write("World");
+            Write("!");
+            ReadLine();
+        }
+    }
+}
