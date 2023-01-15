518e8a86c86d11f8ade0fd2cb1d61cea5ac18641
ToughSpirit <greeeennn@gmail.com>
2023-01-15 19:38:34 +0600

    folder optimization

---
+++ Homework1/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Hell");
+            Write("o, ");
+            Write("World");
+            Write("!");
+            ReadLine();
+        }
+    }
+}
