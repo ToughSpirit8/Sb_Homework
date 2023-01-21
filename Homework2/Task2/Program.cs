25650eb9de2599008792c097e97c075eabdf348e
ToughSpirit <greeeennn@gmail.com>
2023-01-21 12:05:34 +0600

    v1.5

--- Homework2/Task2/Program.cs
+++ Homework2/Task2/Program.cs
@@ -8,8 +8,7 @@
             double mathPoints = 5.5;
             double sumOfPoints = mathPoints + programmingPoints;
             double midArithmetic = sumOfPoints / 2;
-            Console.WriteLine($"{"Points for programming:",23}{programmingPoints}\n{"Points for math:",23}{mathPoints}\nPress enter to get arithmetic mean...");
-            Console.WriteLine($"{"Sum of Points is:"}{sumOfPoints}\nArithmetic mean of Points is: {midArithmetic}\nPress enter for pattern formating...");
+            Console.WriteLine($"{"Points for programming:",23}{programmingPoints}\n{"Points for math:",23}{mathPoints}\n{"Sum of Points is:"}{sumOfPoints}\nArithmetic mean of Points is: {midArithmetic}");
             Console.ReadLine();
             string Pattern = "Points for programming:{0}\nPoints for math:{1}\nSum of Points is:{2}\nArithmetic mean of Points is: {3}\nPress enter to quit...";
             Console.WriteLine(Pattern, programmingPoints, mathPoints, sumOfPoints, midArithmetic);
