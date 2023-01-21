aab3b3d3494c87d5743caa1740b4958f38293dba
ToughSpirit <greeeennn@gmail.com>
2023-01-21 10:52:11 +0600

    v1.5

---
+++ Homework2/Task/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HomeWork2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string fullName = "Vladimir Dracula Cepish";
+			byte age = 254;
+			string email = "vladimir1752@dracula.com";
+			double programmingPoints = 1.1;
+			double mathPoints = 5.5;
+			double physicsPoints = 10.10;
+			Console.WriteLine($"{"Name:",23} {fullName}\n{"Age:",23} {age}\n{"Email:",23} {email}\n{"Points for Programming:",23} {programmingPoints}\n{"Points for Math:",23} {mathPoints}\n{"Points for Physics:",23} {physicsPoints}");
+            Console.ReadLine();
+		}
+
+
+	}
+}
