c91443e3af4fc69168240406a00207b39b094d0c
Greeeennn@gmail.com <Greeeennn@gmail.com>
2023-01-24 10:52:14 +0600

    Evrything is working but large numbers arent

---
+++ Homework/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using System.Transactions;
+using static System.Console;
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Input your number...");
+            int number = BigInteger.Parse(ReadLine();
+            number = (int)(long)number;
+            if (number == 0) WriteLine("Zero is uneven");
+            else if (number % 2 == 0) WriteLine("Entered number is even");
+            else WriteLine("Entered number is uneven");
+
+
+
+
+
+        }
+    }
+}
