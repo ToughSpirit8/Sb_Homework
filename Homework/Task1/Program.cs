59608f69b7cdbd8c82715cd061b7a4b8219748f3
Greeeennn@gmail.com <Greeeennn@gmail.com>
2023-01-24 11:17:39 +0600

    Big.integer added for numbrrs ofer 100500

--- Homework/Task1/Program.cs
+++ Homework/Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Transactions;
 using static System.Console;
 namespace Task1
@@ -8,16 +9,11 @@ namespace Task1
         static void Main(string[] args)
         {
             WriteLine("Input your number...");
-            int number = BigInteger.Parse(ReadLine();
-            number = (int)(long)number;
-            if (number == 0) WriteLine("Zero is uneven");
-            else if (number % 2 == 0) WriteLine("Entered number is even");
+            string number = ReadLine();  //получаем любую строку от юзера
+            var number1 = BigInteger.Parse(number); //конвертируем в число (BigInteger используем для чисел любой длинны)
+            //if (number1 == 0) WriteLine("Zero is uneven");
+            if (number1 % 2 == 0) WriteLine("Entered number is even");
             else WriteLine("Entered number is uneven");
-
-
-
-
-
         }
     }
 }
