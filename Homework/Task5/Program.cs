57715fc44409583fea3ba48654eb42b70a192909
Greeeennn@gmail.com <Greeeennn@gmail.com>
2023-01-24 21:54:00 +0600

    task5

---
+++ Homework/Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            {
+                WriteLine("Введите максимальное число диапозона от 0 до...");
+                int lastValue = int.Parse(ReadLine());
+                int solution = new Random().Next(0, lastValue + 1);
+                //WriteLine(solution); //чит код)
+                string guessValue = "-1";
+                WriteLine("Для выхода нажмите enter в пустой строке");
+                WriteLine("Введите загаданное число");
+                while (solution != int.Parse(guessValue))
+                {
+                    guessValue = ReadLine();
+                    if (guessValue == "") { WriteLine($"Жаль, загаданное число было {solution}. Покаки!"); Environment.Exit(1); }
+                    else if (int.Parse(guessValue) > solution) { WriteLine($"Загаданное число меньше {guessValue}"); }
+                    else if (int.Parse(guessValue) < solution) { WriteLine($"Загаданное число больше {guessValue}"); }
+                    
+                }
+                WriteLine($"Вы угадали! Загаданное число было {guessValue}");
+                Main();
+            }
+        }
+    }
+}
