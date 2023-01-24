5b4028a401c7fd0eef22442941414241cf657f89
Greeeennn@gmail.com <Greeeennn@gmail.com>
2023-01-24 21:21:37 +0600

    task4

---
+++ Homework/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            {
+                WriteLine("Введите длинну последовательности");
+                int numLength = int.Parse(ReadLine()); 
+                int minValue = int.MaxValue;
+                WriteLine("Введите числа из последовательности");
+                for (int i = numLength; i > 0; i--)
+                {
+                    int currentNumber = int.Parse(ReadLine());
+                    if (currentNumber < minValue) { minValue= currentNumber;}  
+                }
+            
+                WriteLine($"Минимальное значение в заданной последовательности {minValue}");
+            }
+        }
+    }
+}
