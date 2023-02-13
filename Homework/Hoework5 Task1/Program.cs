f0de47bcf2bb8c12824cc32f54d69738f3959233
Greeeennn@gmail.com <Greeeennn@gmail.com>
2023-02-13 18:14:51 +0600

    Hw5 task1

---
+++ Homework/Hoework5 Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+namespace Hoework5_Task1
+{
+    internal class Program
+    {
+        public static string[] SplitText(string input)
+        {
+            
+            string[] StringMassive =  input.Split(' ');
+            return StringMassive;
+            
+        }
+        static void PrintWords(string[] subs)
+        {
+            foreach (var sub in subs)
+                WriteLine($"Substring: {sub}");
+        }
+        static public void Main()
+        {
+            
+            Write("Введите предложение:");
+            string? text = ReadLine();
+            if (text == null | text == "") { Main(); }
+            else  { PrintWords(SplitText(text)); }
+            
+        }       
+    }
+}
